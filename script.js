c4b3d0460f0110b65102fed001128f153d5671ca
Natnael Dejene <thecodegeek1@gmail.com>
2025-03-17 21:53:04 +0300

    make error message disapear

--- script.js
+++ script.js
@@ -11,19 +11,25 @@ document.addEventListener('submit', (e) => {
     const email = input.value;
     const pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
+
+    const error = document.createElement('p');
+    error.textContent = 'Please provide a valid email address'
+    error.className = 'error';
+
     if (!pattern.test(email) && !isShown) {
         isShown = true;
         input.style.borderColor = 'hsl(354, 100%, 66%)';
-        const error = document.createElement('p');
-        error.textContent = 'Please provide a valid email address'
-        error.className = 'error';
-        console.log(error);
 
         if (window.innerWidth >= 1024) {
             container.insertBefore(error, illustration);
         } else {
             form.insertBefore(error, button);
         }
+    } else if (pattern.test(email) && isShown) {
+        document.querySelector('.error').remove();
+        input.style.borderColor = 'hsl(223, 100%, 88%)';
+        input.value = '';
+        isShown = false;
     }
 
 })
