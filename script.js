97f50bd414a23b195c8be4f251a6de52908d0927
Natnael Dejene <thecodegeek1@gmail.com>
2025-03-18 07:07:17 +0300

    add functions to display and remove error message

--- script.js
+++ script.js
@@ -3,33 +3,47 @@ const input = document.querySelector('input');
 const form = document.querySelector('form');
 const illustration = document.querySelector('.illustration');
 const container = document.querySelector('.container');
-let isShown = false;
+const error = document.createElement('p');
+error.className = 'error';
 
 document.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const email = input.value;
-    const pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-
-    const error = document.createElement('p');
-    error.textContent = 'Please provide a valid email address'
-    error.className = 'error';
-
-    if (!pattern.test(email) && !isShown) {
-        isShown = true;
-        input.style.borderColor = 'hsl(354, 100%, 66%)';
-
-        if (window.innerWidth >= 1024) {
-            container.insertBefore(error, illustration);
-        } else {
-            form.insertBefore(error, button);
-        }
-    } else if (pattern.test(email) && isShown) {
-        document.querySelector('.error').remove();
-        input.style.borderColor = 'hsl(223, 100%, 88%)';
+    if (email === '' || email === null) {
+        showError('Whoops! It looks like you forgot to add your email');
+    } else if (!isValid(email)) {
+        showError('Please provide a valid email address');
+    } else if (isValid(email)) {
         input.value = '';
-        isShown = false;
+        removeError();
     }
 
 })
+
+function isValid(email) {
+    const pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    return pattern.test(email);
+
+}
+
+function showError(message) {
+
+    error.textContent = message;
+    input.style.borderColor = 'hsl(354, 100%, 66%)';
+
+    if (window.innerWidth >= 1024) {
+        container.insertBefore(error, illustration);
+    } else {
+        form.insertBefore(error, button);
+    }
+
+}
+
+function removeError() {
+    error.remove();
+    input.style.borderColor = 'hsl(223, 100%, 88%)';
+    input.value = '';
+}
